09280bf5d57b9957f110f670ab8700aa2311c265
Dlrjswn <lkj6387@gmail.com>
2025-07-02 20:47:01 +0900

    Comment 엔티티 수정

---
+++ src/main/java/com/example/community/domain/post/entity/Comment.java
@@ -0,0 +1,26 @@
+package com.example.community.domain.post.entity;
+
+import com.example.community.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.auditing.config.AuditingConfiguration;
+
+@Entity
+@Builder
+@EntityListeners(AuditingConfiguration.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+}
