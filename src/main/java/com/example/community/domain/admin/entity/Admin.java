2bf27a86cfaa288c476ed662ac30ddbdc94c0a71
Dlrjswn <lkj6387@gmail.com>
2025-07-02 20:03:47 +0900

    Admin 엔티티 생성

--- src/main/java/com/example/community/domain/admin/entity/Admin.java
+++ src/main/java/com/example/community/domain/admin/entity/Admin.java
@@ -2,13 +2,30 @@ package com.example.community.domain.admin.entity;
 
 import com.example.community.global.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.auditing.config.AuditingConfiguration;
 
 @Entity
 @EntityListeners(AuditingConfiguration.class)
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
