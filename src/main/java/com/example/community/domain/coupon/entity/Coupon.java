c7abbb7e0a18e07dc1718c03c3afa83f38628ad8
Dlrjswn <lkj6387@gmail.com>
2025-07-02 20:40:53 +0900

    Coupon 엔티티 생성

--- src/main/java/com/example/community/domain/coupon/entity/Coupon.java
+++ src/main/java/com/example/community/domain/coupon/entity/Coupon.java
@@ -1,4 +1,42 @@
 package com.example.community.domain.coupon.entity;
 
-public class Coupon {
+import com.example.community.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.auditing.config.AuditingConfiguration;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@EntityListeners(AuditingConfiguration.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Coupon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+    @Column(nullable = false)
+    private int validDays;
+
+    @Column(nullable = false)
+    private int maxAmount;
+
+    @Column(nullable = false)
+    private int currentAmount;
 }
