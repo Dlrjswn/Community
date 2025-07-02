c1e333f59ff01c019b7244785109f53e93859863
Dlrjswn <lkj6387@gmail.com>
2025-07-02 20:50:16 +0900

    PostImage 엔티티 생성

---
+++ src/main/java/com/example/community/domain/post/entity/PostImage.java
@@ -0,0 +1,23 @@
+package com.example.community.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class PostImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+}
