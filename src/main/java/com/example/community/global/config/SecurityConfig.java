92f46a3ff1fcebbf5df09c4995c4206fe1440d01
Dlrjswn <lkj6387@gmail.com>
2025-08-04 15:34:41 +0900

    securityConfig 수정

--- src/main/java/com/example/community/global/config/SecurityConfig.java
+++ src/main/java/com/example/community/global/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(("api/v1/coupon/issueTest")).permitAll()
                         .requestMatchers(("api/v1/post/saveTest")).permitAll()
+                        .requestMatchers(("api/v1/post/generateTestPosts")).permitAll()
                         .requestMatchers(("api/v1/post/detail")).permitAll()
                         .requestMatchers(("api/v1/post/list")).permitAll()
                         .requestMatchers(("api/v1/post/search")).permitAll()
